f63fdd16421b11e9eb572445e0c77cf68e34cec2
EC2 Default User <ec2-user@ip-172-31-22-23.us-east-2.compute.internal>
2021-09-15 01:46:06 +0000

    Finished all Parts in hw-ruby-intro

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -29,25 +29,46 @@ def sum_to_n? arr, n
   if arr.length == 1
     return false
   end
-  !!arr.uniq.combination(2).detect { |a, b| a + b == n }
+  !!arr.uniq.combination(2).detect { |x, y| x + y == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  return (s.match(/^[^aeiou\W]/i))
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  #if (s.match(/\D/i))
+  #  return false
+  #end
+  if (s.match(/[^01]/) || s.empty?)
+    return false
+  end
+  
+  return ((s.to_i(base=2) % 4) == 0)
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError.new("Invalid argument.") if isbn.empty? or price <= 0.0
+    @isbn = isbn
+    @price = price
+  end
+  
+  attr_accessor :isbn, :price
+  
+  def price_as_string
+    return "$%0.2f" % price
+  end
+  
 end
